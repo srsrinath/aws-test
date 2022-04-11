be1c4e021b6543ba5c9616ced7bc4f12f11d29b4
srsrinath2 <s.r.srinath8@gmail.com>
2022-04-11 19:14:42 +0530

    intial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ct9xxou6nafq.ap-southeast-1.rds.amazonaws.com"
+customuser = "user"
+custompass = "charantej"
+customdb = "employee"
+custombucket = "adddetails"
+customregion = "ap-southeast-1"
